45970dbc85c7393e24224cb7f38ef25d6acd66fc
Giovanna-Gregorio <giovanna_gregorio@univem.edu.br>
2020-09-18 20:03:08 -0300

    Adicionando pacote de validação e classe de imoveis

---
+++ Shared/Imovel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared
+{
+    public class Imovel
+    {
+        public int IdImovel { get; set; }
+        [Required]
+        public int IdTipoImovel { get; set; }
+        public int IdPessoa { get; set; }
+        public int Matricula { get; set; }
+        public string Descricao { get; set; }
+        public string Logradoudo { get; set; }
+        public string Bairro { get; set; }
+        public int Numero { get; set; }
+        public string Cidade { get; set; }
+        public string Complemento { get; set; }
+        public string UF { get; set; }
+        public double Valor { get; set; }
+        public string Status { get; set; }
+    }
+}
