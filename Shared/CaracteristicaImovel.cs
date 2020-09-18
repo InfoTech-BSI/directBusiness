e9ff63f74c1f3d03805c9b0494d03d104c1db6b8
Giovanna-Gregorio <giovanna_gregorio@univem.edu.br>
2020-09-18 20:50:31 -0300

    Adicionando entidades

---
+++ Shared/CaracteristicaImovel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared
+{
+    public class CaracteristicaImovel
+    {
+        public int IdCarac { get; set; }
+        public int IdImovel { get; set; }
+        [Required(ErrorMessage = "Quantidade de quartos é obrigatório")] 
+        public int QtdeQuartos { get; set; }
+        [Required(ErrorMessage = "Quantidade de Banheiros é obrigatório")] 
+        public int QtdeBanheiros { get; set; }
+        [Required(ErrorMessage = "Estacionamento é obrigatório")] 
+        public bool Estacionamento { get; set; }
+        [Required(ErrorMessage = "Metragem do imóvel é obrigatório")] 
+        public double Metragem { get; set; }
+        [Required(ErrorMessage = "Pets é obrigatório")] 
+        public bool Pets { get; set; }
+        [Required(ErrorMessage = "Mobiliado é obrigatório")] 
+        public bool Mobiliado { get; set; }
+        [Required(ErrorMessage = "Apartamento é obrigatório")] 
+        public bool Apartamento { get; set; }
+    }
+}
