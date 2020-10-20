04fb97648abd1192ddd63487750ef16d90668bec
Geraldo Junior <geraldojunioroficial@gmail.com>
2020-10-19 21:37:41 -0300

    Criação da estrutura da tela de cadastro de pessoas

--- Client/wwwroot/js/app.js
+++ Client/wwwroot/js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(document).on('input', '#valorBusca', function() {
         $('#valorExibido').html("Busca a partir 100 reais até " + $(this).val() + " reais.");
     });
-    
+
     $(document).on('click', '#Comprar', function() {
         $( "#Comprar" ).toggleClass( "active" );
         $( "#Comprar" ).toggleClass( "disabled" );
@@ -16,19 +16,40 @@ $(document).ready(function(){
         $("#valorBusca").attr("max", 2000000);
         $("#valorBusca").val(300000);
     });
-    
+
     $(document).on('click', '#Alugar', function() {
         $( "#Comprar" ).toggleClass( "active" );
         $( "#Comprar" ).toggleClass( "disabled" );
         $( "#Alugar" ).toggleClass( "active" );
         $( "#Alugar" ).toggleClass( "disabled" );
-        $('#valorMinimo').html("100 reais");
-        $('#valorExibido').html("Busca a partir 100 reais até 2500 reais.");
-        $('#valorMaximo').html("5.000 reais");
-        $("#valorBusca").attr("step", 100);
-        $("#valorBusca").attr("min", 100);
-        $("#valorBusca").attr("max", 5000);
+        $('#valorMinimo').html("500 reais");
+        $('#valorExibido').html("Busca a partir 500 reais até 2500 reais.");
+        $('#valorMaximo').html("15.000 reais");
+        $("#valorBusca").attr("step", 200);
+        $("#valorBusca").attr("min", 500);
+        $("#valorBusca").attr("max", 15000);
         $("#valorBusca").val(2500);
+    });
+
+    $(document).on('change', "#valorBusca", function() {
+        if( $( "#valorBusca" ).val() == 14900 ){
+            $( "#valorBusca" ).val(15000);
+            $( "#valorExibido" ).html("Busca a partir 100 reais até " + 15000 + " reais.");
+        }
+    });
+
+    $(document).on('click', '#CriarConta', function() {
+        $("#loginModal").modal('hide');
+    });
+
+    $(document).on('click', '.verSenha', function() {
+        if( $(".verSenha .senha").first().text() == 'Ver senha' ){
+            $(".verSenha .senha").text('Esconder senha');
+            $(".senha").attr("type","word");
+        }else{
+            $(".verSenha .senha").text('Ver senha');
+            $(".senha").attr("type","password");
+        }
     });
 
     //Deixado para uso futuro
@@ -49,4 +70,16 @@ $(document).ready(function(){
     //         console.log("Carregou3333333333");
     //     });
     // });
+});
+
+//Funções máscara
+//https://nosir.github.io/cleave.js/
+
+$(document).ready(function() {
+    console.log("Terminou");
+    const cleave = new Cleave('.input-cpf', {
+        delimiters: ['.', '.', '-'],
+        blocks: [3, 3, 3, 2],
+        uppercase: true
+    });
 });
