cae2dbcfb992eab5615e15e613df293214722abc
OfGeraldo <geraldojunioroficial@gmail.com>
2020-10-24 15:26:08 -0300

    Finalizado ajax máscara CEP

--- Client/wwwroot/js/app.js
+++ Client/wwwroot/js/app.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
-    $(document).on('input', '#valorBusca', function() {
+$(document).ready(function () {
+    $(document).on('input', '#valorBusca', function () {
         $('#valorExibido').html("Busca a partir 100 reais até " + $(this).val() + " reais.");
     });
 
-    $(document).on('click', '#Comprar', function() {
-        $( "#Comprar" ).toggleClass( "active" );
-        $( "#Comprar" ).toggleClass( "disabled" );
-        $( "#Alugar" ).toggleClass( "active" );
-        $( "#Alugar" ).toggleClass( "disabled" );
+    $(document).on('click', '#Comprar', function () {
+        $("#Comprar").toggleClass("active");
+        $("#Comprar").toggleClass("disabled");
+        $("#Alugar").toggleClass("active");
+        $("#Alugar").toggleClass("disabled");
         $('#valorMinimo').html("5.000 reais");
         $('#valorExibido').html("Busca a partir 5.000 reais até 300000 reais.");
         $('#valorMaximo').html("2.000.000 reais");
@@ -17,11 +17,11 @@ $(document).ready(function(){
         $("#valorBusca").val(300000);
     });
 
-    $(document).on('click', '#Alugar', function() {
-        $( "#Comprar" ).toggleClass( "active" );
-        $( "#Comprar" ).toggleClass( "disabled" );
-        $( "#Alugar" ).toggleClass( "active" );
-        $( "#Alugar" ).toggleClass( "disabled" );
+    $(document).on('click', '#Alugar', function () {
+        $("#Comprar").toggleClass("active");
+        $("#Comprar").toggleClass("disabled");
+        $("#Alugar").toggleClass("active");
+        $("#Alugar").toggleClass("disabled");
         $('#valorMinimo').html("500 reais");
         $('#valorExibido').html("Busca a partir 500 reais até 2500 reais.");
         $('#valorMaximo').html("15.000 reais");
@@ -31,45 +31,26 @@ $(document).ready(function(){
         $("#valorBusca").val(2500);
     });
 
-    $(document).on('change', "#valorBusca", function() {
-        if( $( "#valorBusca" ).val() == 14900 ){
-            $( "#valorBusca" ).val(15000);
-            $( "#valorExibido" ).html("Busca a partir 100 reais até " + 15000 + " reais.");
+    $(document).on('change', "#valorBusca", function () {
+        if ($("#valorBusca").val() == 14900) {
+            $("#valorBusca").val(15000);
+            $("#valorExibido").html("Busca a partir 100 reais até " + 15000 + " reais.");
         }
     });
 
-    $(document).on('click', '#CriarConta', function() {
+    $(document).on('click', '#CriarConta', function () {
         $("#loginModal").modal('hide');
     });
 
-    $(document).on('click', '.verSenha', function() {
-        if( $(".verSenha .senha").first().text() == 'Ver senha' ){
+    $(document).on('click', '.verSenha', function () {
+        if ($(".verSenha .senha").first().text() == 'Ver senha') {
             $(".verSenha .senha").text('Esconder senha');
-            $(".senha").attr("type","word");
-        }else{
+            $(".senha").attr("type", "word");
+        } else {
             $(".verSenha .senha").text('Ver senha');
-            $(".senha").attr("type","password");
+            $(".senha").attr("type", "password");
         }
     });
-
-    //Deixado para uso futuro
-
-    // window.addEventListener("load", function(event) {
-    //     $( "#stepper" ).on('load', function(){
-    //         console.log("Carregou1111111111111");
-    //     });
-    // });
-    // $ (window) .on ('load', function () {
-    //     console.log("Carregou1111111111111");
-    //     $( ".bs-stepper" ).on ('load', function(){
-    //         console.log("Carregou22222222222222");
-    //         var stepper = new Stepper(document.querySelector('#stepper'));
-    //     });
-
-    //     $(window).on('.bs-stepper', 'ready' ,function() {
-    //         console.log("Carregou3333333333");
-    //     });
-    // });
 });
 
 //Funções máscara
@@ -78,16 +59,16 @@ $(document).ready(function(){
 
 // máscara CPF
 
-$(document).on('keypress', ".input-cpf", function() {
+$(document).on('keyup', ".input-cpf", function () {
     console.log("Ativou máscara CPF");
     const cleave = new Cleave('.input-cpf', {
         delimiters: ['.', '.', '-'],
         blocks: [3, 3, 3, 2],
         numericOnly: true
     });
-    $(document).off('keypress', '.input-cpf');
+    $(document).off('keyup', '.input-cpf');
 });
-$(document).on('focusout', ".input-cpf", function() {
+$(document).on('focusout', ".input-cpf", function () {
     console.log("Ativou máscara CPF");
     const cleave = new Cleave('.input-cpf', {
         delimiters: ['.', '.', '-'],
@@ -99,16 +80,16 @@ $(document).on('focusout', ".input-cpf", function() {
 
 // máscara RG
 
-$(document).on('keypress', ".input-rg", function() {
+$(document).on('keyup', ".input-rg", function () {
     console.log("Ativou máscara RG");
     const cleave = new Cleave('.input-rg', {
         delimiters: ['.', '.', '-'],
         blocks: [2, 3, 3, 1],
         uppercase: true
     });
-    $(document).off('keypress', '.input-rg');
+    $(document).off('keyup', '.input-rg');
 });
-$(document).on('focusout', ".input-rg", function() {
+$(document).on('focusout', ".input-rg", function () {
     console.log("Ativou máscara RG");
     const cleave = new Cleave('.input-rg', {
         delimiters: ['.', '.', '-'],
@@ -118,22 +99,45 @@ $(document).on('focusout', ".input-rg", function() {
     $(document).off('focusout', '.input-rg');
 });
 
-// máscara CEP
+// máscara e busca CEP 
 
-$(document).on('keypress', ".input-cep", function() {
+function buscaCEP() {
+
+    const cep = $('.input-cep').val();
+
+    $('.icone-cep').html(`
+    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    <span class="sr-only">Carreando...</span>
+    `);
+
+    $.get("https://viacep.com.br/ws/" + cep + "/json/", function (data) {
+        console.log(data);
+        $('#Endereco').val(data.logradouro);
+        $('#Bairro').val(data.bairro);
+        $('#Cidade').val(data.localidade);
+        $('#Complemento').val(data.complemento);
+        $('#UF').val(data.uf);
+    })
+        .fail(function () {
+            alert("Erro ao procurar cep");
+        }).always(function () {
+            $('.icone-cep').html(`<i class="fas fa-search"></i>`);
+        });
+};
+$(document).on('keyup', ".input-cep", function () {
     console.log("Ativou máscara CEP");
     const cleave = new Cleave('.input-cep', {
-        delimiters: ['.', '-'],
-        blocks: [2, 3, 2],
+        delimiters: ['-'],
+        blocks: [5, 3],
         numericOnly: true
     });
-    $(document).off('keypress', '.input-cep');
+    if( $(this).val().length == 9) buscaCEP();
 });
-$(document).on('focusout', ".input-cep", function() {
+$(document).on('focusout', ".input-cep", function () {
     console.log("Ativou máscara CEP");
     const cleave = new Cleave('.input-cep', {
-        delimiters: ['.', '-'],
-        blocks: [2, 3, 2],
+        delimiters: ['-'],
+        blocks: [5, 3],
         numericOnly: true
     });
     $(document).off('focusout', '.input-cep');
@@ -141,43 +145,39 @@ $(document).on('focusout', ".input-cep", function() {
 
 //  máscara Valor
 
-$(document).on('keypress', ".input-valor", function() {
+$(document).on('keyup', ".input-valor", function () {
     console.log("Ativou máscara Valor");
     const cleave = new Cleave('.input-valor', {
         numeral: true,
-        numeralThousandsGroupStyle : 'thousand',
-        numeralDecimalScale : 2,
-        numeralPositiveOnly : true,
         numeralDecimalMark: ',',
-        delimiter: '.'
+        delimiter: '.',
+        numeralPositveOnly: true
     });
-    $(document).off('keypress', '.input-valor');
+    $(document).off('keyup', '.input-valor');
 });
-$(document).on('focusout', ".input-valor", function() {
+$(document).on('focusout', ".input-valor", function () {
     console.log("Ativou máscara Valor");
     const cleave = new Cleave('.input-valor', {
         numeral: true,
-        numeralThousandsGroupStyle : 'thousand',
-        numeralDecimalScale : 2,
-        numeralPositiveOnly : true,
         numeralDecimalMark: ',',
-        delimiter: '.'
+        delimiter: '.',
+        numeralPositveOnly: true
     });
     $(document).off('focusout', '.input-valor');
 });
 
 // máscara Celular
 
-$(document).on('keypress', ".input-celular", function() {
+$(document).on('keyup', ".input-celular", function () {
     console.log("Ativou máscara Celular");
     const cleave = new Cleave('.input-celular', {
         numericOnly: true,
         blocks: [0, 3, 5, 4],
         delimiters: ["(", ")", "-"]
     });
-    $(document).off('keypress', '.input-celular');
+    $(document).off('keyup', '.input-celular');
 });
-$(document).on('focusout', ".input-celular", function() {
+$(document).on('focusout', ".input-celular", function () {
     console.log("Ativou máscara Celular");
     const cleave = new Cleave('.input-celular', {
         numericOnly: true,
